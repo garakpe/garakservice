2fe78c93bc1d9a7b9fbffaf1e3f20c632ba339aa
smartchoi <garak.pe@gmail.com>
2024-02-25 15:50:54 +0900

    업로드키 재신청

--- lib/firstpage.dart
+++ lib/firstpage.dart
@@ -7,6 +7,7 @@ class FirstPage extends StatefulWidget {
   const FirstPage({Key? key}) : super(key: key);
 
   @override
+  // ignore: library_private_types_in_public_api
   _FirstPageState createState() => _FirstPageState();
 }
 
